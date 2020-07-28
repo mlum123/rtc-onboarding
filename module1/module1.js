7db73017530d71e0d591ce9a444023854f2c5258
Michelle Lum <michellelum123@gmail.com>
2020-07-28 00:33:32 -0700

    styling

--- module1/module1.js
+++ module1/module1.js
@@ -1,89 +1,81 @@
-const instructions = document.getElementById('instructions')
-const goToScenarioButton = document.getElementById('go-to-scenario-button')
-const gotItButton = document.getElementById('got-it-button')
+const startPage = document.getElementById('start');
+const scenarioPage = document.getElementById('questions');
+const explanationPage = document.getElementById('explanation');
+const finishPage = document.getElementById('finish');
 
+const goToScenarioButton = document.getElementById('go-to-scenario-button');
+const gotItButton = document.getElementById('got-it-button');
 
-const startPage = document.getElementById('start')
-const scenarioPage = document.getElementById('questions')
-const explanationPage = document.getElementById('explanation')
-const finishPage = document.getElementById('finish')
+const scenarioDescription = document.getElementById('scenario');
+const answerButtonsElement = document.getElementById('options');
 
-const scenarioDescription = document.getElementById('scenario')
-const answerButtonsElement = document.getElementById('options')
+const ifAnswerCorrect = document.getElementById('if-correct');
+const answerExplanation = document.getElementById('scenario-explanation');
 
-const ifAnswerCorrect = document.getElementById('if-correct')
-const answerExplanation = document.getElementById('scenario-explanation')
-
-let questionIndex = 0
+let questionIndex = 0;
 
 goToScenarioButton.addEventListener('click', () => {
-	showScenario(scenarios[questionIndex])
+	showScenario(scenarios[questionIndex]);
 })
 
 gotItButton.addEventListener('click', () => {
-	questionIndex ++
-	if (questionIndex <scenarios.length){
-		nextQuestion(scenarios[questionIndex])
+	questionIndex++;
+
+	if (questionIndex < scenarios.length) {
+		nextQuestion(scenarios[questionIndex]);
 	}
-	else{
-		explanationPage.classList.add('hide')
-		finishPage.classList.remove('hide')
+	else {
+		explanationPage.classList.add('hide');
+		finishPage.classList.remove('hide');
 	}
 })
-	
 
-function startModule(){
-	startPage.classList.add('hide')
-	scenarioPage.classList.remove('hide')
-	nextButton.classList.remove('hide')
-}
-
-function showScenario(scenario){
-	resetState()
-	instructions.classList.add('hide')
-	goToScenarioButton.classList.add('hide')
-	scenarioPage.classList.remove('hide')
-	scenarioDescription.innerText = scenario.question
+function showScenario(scenario) {
+	resetState();
+	startPage.classList.add('hide');
+	goToScenarioButton.classList.add('hide');
+	scenarioPage.classList.remove('hide');
+	scenarioDescription.innerText = scenario.question;
 	scenario.answers.forEach(answer => {
-		const button = document.createElement('button')
-		button.innerText = answer.text
-		button.classList.add('btn')
-		if (answer.correct){
-			button.dataset.correct = answer.correct
+		const button = document.createElement('button');
+		button.innerText = answer.text;
+		button.classList.add('btn');
+		if (answer.correct) {
+			button.dataset.correct = answer.correct;
 		}
-		button.addEventListener('click', selectAnswer)
-		answerButtonsElement.appendChild(button)
+		button.addEventListener('click', selectAnswer);
+		answerButtonsElement.appendChild(button);
 	})
 }
 
-function selectAnswer(e){
-	const selectedButton = e.target
-	const correct = selectedButton.dataset.correct
-	if (correct){
-		ifAnswerCorrect.innerText = 'You are right!'
+function selectAnswer(e) {
+	const selectedButton = e.target;
+	const correct = selectedButton.dataset.correct;
+	if (correct) {
+		ifAnswerCorrect.innerText = 'You are right!';
 	}
-	else{
-		ifAnswerCorrect.innerText = 'Oops! You missed the correct answer'
+	else {
+		ifAnswerCorrect.innerText = 'Oops! You missed the correct answer';
 	}
-	showExplanation(explanations[questionIndex])
+	showExplanation(explanations[questionIndex]);
 }
 
-function showExplanation(explanation){
-	scenarioPage.classList.add('hide')
-	explanationPage.classList.remove('hide')
-	gotItButton.classList.remove('hide')
-	answerExplanation.innerText = explanation
+function showExplanation(explanation) {
+	scenarioPage.classList.add('hide');
+	explanationPage.classList.remove('hide');
+	gotItButton.classList.remove('hide');
+	answerExplanation.innerText = explanation;
 }
 
-function nextQuestion(scenario){
-	gotItButton.classList.add('hide')
-	explanationPage.classList.add('hide')
-	showScenario(scenario)
+function nextQuestion(scenario) {
+	gotItButton.classList.add('hide');
+	explanationPage.classList.add('hide');
+	showScenario(scenario);
 }
 
-function resetState(){
-	while (answerButtonsElement.firstChild){
-		answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+function resetState() {
+	while (answerButtonsElement.firstChild) {
+		answerButtonsElement.removeChild(answerButtonsElement.firstChild);
 	}
 }
 
@@ -103,7 +95,7 @@ const scenarios = [
 		]
 	},
 	{
-		question:'  What’s a great way to stand out to your host?',
+		question:'What’s a great way to stand out to your host?',
 		answers:[
 			{text: 'Show up late', correct: false},
 			{text: 'Forget to attend', correct: false},
@@ -115,5 +107,5 @@ const scenarios = [
 
 const explanations = ['Breaking your word is disrespectful of others’ time and breaks their confidence in you and RTC.  If a problem arises and you can no longer attend the webinar, send an email to the host before it begins so someone else may have the opportunity to attend.',
 					'Showing you are organized, responsible and trustworthy goes very far in creating good working relationships. You should be responsible for holding yourself accountable to attend your engagements.',
-					' This doesn’t happen nearly enough so it will make you stand out in a positive light and expand your network!']
+					'This doesn’t happen nearly enough so it will make you stand out in a positive light and expand your network!']
 
