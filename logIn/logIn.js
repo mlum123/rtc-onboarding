dbef67aeca41b2ff3f7694c6852ecd18c12d4da1
coco <ziyuanli0408@gmail.com>
2020-07-30 23:39:22 -0400

    added log-in/sign-up page and changed dashboard color theme

---
+++ logIn/logIn.js
@@ -0,0 +1,46 @@
+const userNotExistAlert = document.getElementById('user-not-exist-alert');
+const wrongPasswordAlert = document.getElementById('wrong-password-alert');
+
+function logInSuccessful(){
+	
+	for (i = 0; i < users.length; i++){
+		if(document.getElementById("email").value === users[i].email){
+			if(document.getElementById("password").value === users[i].password){
+				return true;
+			}
+			showWrongPasswordMessage();
+			return false;
+		}
+	}
+
+	showUserNotExistMessage();
+	return false;
+}
+
+
+function showWrongPasswordMessage(){
+	userNotExistAlert.classList.add("hide");
+	wrongPasswordAlert.classList.remove("hide");
+}
+
+function showUserNotExistMessage(){
+	userNotExistAlert.classList.remove("hide");
+	wrongPasswordAlert.classList.add("hide");
+}
+
+const users = [
+	{
+		firstName: 'Jessica',
+		lastName: 'Smith',
+		email: 'jessica@gmail.com',
+		password: 'asdfg'
+		
+	},
+	{
+		firstName: 'Jane',
+		lastName: 'Johnson',
+		email: 'jjohnson@gmail.com',
+		password: 'hello'
+	}
+
+]
