dfa017e86e5891a64e2ef5d88538c4420193a2c6
Coco Li <51799822+ziyuanlicoco@users.noreply.github.com>
2020-07-31 01:30:28 -0400

    Update logIn.js

--- logIn.js
+++ logIn.js
@@ -3,25 +3,18 @@ const wrongPasswordAlert = document.getElementById('wrong-password-alert');
 
 
 function logInSuccessful(){
-	//document.getElementById('log-in-button').disabled = true;
-	if(checkLogIn() === 2){
-		
-		showUserNotExistMessage();
-	}
-	else if(checkLogIn() === 1){
-		showWrongPasswordMessage();
-	}
-	else{
-		userNotExistAlert.classList.add("hide");
-		wrongPasswordAlert.classList.add("hide");
-		//document.getElementById('log-in-button').disabled = false;
-		document.getElementById('log-in-button').addEventListener('click',()=>{
-		    
-		    	window.location = '../dashboard.html';
-		}); 
-    		
+	for (i = 0; i < users.length; i++){
+		if(document.getElementById("email").value === users[i].email){
+			if(document.getElementById("password").value === users[i].password){
+				return true;
+			}
+			showWrongPasswordMessage();
+			return false;
+		}
 	}
 
+	showUserNotExistMessage();
+	return false;
 	
 }
 
@@ -34,18 +27,6 @@ function showWrongPasswordMessage(){
 function showUserNotExistMessage(){
 	userNotExistAlert.classList.remove("hide");
 	wrongPasswordAlert.classList.add("hide");
-}
-
-function checkLogIn(){
-	for(i = 0; i < users.length;i++){
-		if (document.getElementById('email').value === users[i].email){
-			if(document.getElementById('password').value === users[i].password){
-				return 0;
-			}
-			return 1;
-		}
-	}
-	return 2;
 }
 
 const users = [
