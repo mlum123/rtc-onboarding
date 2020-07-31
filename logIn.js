e416baf90b417f86bf0b9cdafbcc3ceb9bd81c5f
Coco Li <51799822+ziyuanlicoco@users.noreply.github.com>
2020-07-31 01:22:31 -0400

    Add files via upload

---
+++ logIn.js
@@ -0,0 +1,66 @@
+const userNotExistAlert = document.getElementById('user-not-exist-alert');
+const wrongPasswordAlert = document.getElementById('wrong-password-alert');
+
+
+function logInSuccessful(){
+	//document.getElementById('log-in-button').disabled = true;
+	if(checkLogIn() === 2){
+		
+		showUserNotExistMessage();
+	}
+	else if(checkLogIn() === 1){
+		showWrongPasswordMessage();
+	}
+	else{
+		userNotExistAlert.classList.add("hide");
+		wrongPasswordAlert.classList.add("hide");
+		//document.getElementById('log-in-button').disabled = false;
+		document.getElementById('log-in-button').addEventListener('click',()=>{
+		    
+		    	window.location = '../dashboard.html';
+		}); 
+    		
+	}
+
+	
+}
+
+
+function showWrongPasswordMessage(){
+	userNotExistAlert.classList.add("hide");
+	wrongPasswordAlert.classList.remove("hide");
+}
+
+function showUserNotExistMessage(){
+	userNotExistAlert.classList.remove("hide");
+	wrongPasswordAlert.classList.add("hide");
+}
+
+function checkLogIn(){
+	for(i = 0; i < users.length;i++){
+		if (document.getElementById('email').value === users[i].email){
+			if(document.getElementById('password').value === users[i].password){
+				return 0;
+			}
+			return 1;
+		}
+	}
+	return 2;
+}
+
+const users = [
+	{
+		firstName: 'Jessica',
+		lastName: 'Smith',
+		email: 'jessica@gmail.com',
+		password: 'asdfg'
+		
+	},
+	{
+		firstName: 'Jane',
+		lastName: 'Johnson',
+		email: 'jjohnson@gmail.com',
+		password: 'hello'
+	}
+
+]
---
+++ password_reset.js
@@ -0,0 +1,46 @@
+const userNotExistAlert = document.getElementById("password-not-match-alert");
+const resetSuccessfulPage = document.getElementById("reset-successful");
+const resetHint = document.getElementById("reset-hint");
+const resetForm = document.getElementById("reset-password-form");
+
+function resetSuccessfulShow(){
+
+	if(checkUserExist()){
+		resetSuccessfulPage.classList.remove("hide");
+		resetHint.classList.add("hide");
+		resetForm.classList.add("hide");
+		userNotExistAlert.classList.add("hide");
+	}
+	else{
+		userNotExistAlert.classList.remove("hide");	
+	}
+
+}
+
+function checkUserExist(){
+	for (i = 0; i < users.length; i++){
+		if(document.getElementById("email").value === users[i].email){
+			return true;
+		}
+	}
+	return false;
+
+}
+
+
+const users = [
+	{
+		firstName: 'Jessica',
+		lastName: 'Smith',
+		email: 'jessica@gmail.com',
+		password: 'asdfg'
+		
+	},
+	{
+		firstName: 'Jane',
+		lastName: 'Johnson',
+		email: 'jjohnson@gmail.com',
+		password: 'hello'
+	}
+
+]
